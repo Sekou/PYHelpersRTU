448436d2535aa13a01afdb83c77626429382b397
Sekou <sekoudiane1990@gmail.com>
2024-09-28 10:29:32 +0300

    Extend and rename text3d.py to tex3d.py
    Colored transparent textures creation and faces rendering

--- tex3d.py
+++ tex3d.py
@@ -1,10 +1,11 @@
 #S. Diane, 2024
-#Drawing text in PyGame+OpenGL simulation
+#Drawing text and color textures in PyGame+OpenGL simulation
 
 import pygame
 from OpenGL.GL import *
 
 texTable={}
+colorTable={}
 PM, MVM = None, None #projection and modelview matrices
 def pushGL():
     global PM, MVM
@@ -17,19 +18,19 @@ def popGL():
     glMatrixMode(GL_MODELVIEW)
     glLoadMatrixf(MVM)
 
-def enableTex(display):
+def enableTex(WH):
     glEnable(GL_TEXTURE_2D)
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
     glEnable(GL_BLEND)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    glOrtho(0, display[0], display[1], 0, -100, 100)
+    glOrtho(0, WH[0], WH[1], 0, -100, 100)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-    glViewport(0, 0, display[0],  display[1])
+    glViewport(0, 0, WH[0],  WH[1])
 
 def genTextureForText(text):
-    if text in texTable.keys(): return
+    if text in texTable.keys(): return texTable[text][0]
     font = pygame.font.Font(None, 64)
     textSurface = font.render(text, True, (0,0,0, 255))
     szx, szy = textSurface.get_width(), textSurface.get_height()
@@ -42,14 +43,32 @@ def genTextureForText(text):
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE)
     texTable[text] = (i, text)
+    return i
+
+def genColorTexture(color):
+    sc=str(color)
+    if sc in colorTable.keys(): return colorTable[sc][0]
+    szx, szy = 4, 4
+    textSurface = pygame.Surface([szx, szy], pygame.SRCALPHA)
+    textSurface.fill(color)
+    image = pygame.image.tostring(textSurface, "RGBA", True)
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+    i = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D, i)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, szx, szy, 0, GL_RGBA, GL_UNSIGNED_BYTE, image)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE)
+    colorTable[sc] = (i, color)
+    return i
 
 def drawTextInner(displayWH, text, sz, x, y):
-    genTextureForText(text)
+    i=genTextureForText(text)
     k=displayWH[0]/displayWH[1]
     w,h=len(text)*sz/k, sz*k
     x,y=-1+2*x,1-2*y
     glLoadIdentity()
-    glBindTexture(GL_TEXTURE_2D, texTable[text][0])
+    glBindTexture(GL_TEXTURE_2D, i)
     glBegin(GL_QUADS)
     for ix,iy in [[0,0], [1,0], [1,1], [0,1]]:
         glTexCoord2f(ix,iy)
@@ -64,3 +83,19 @@ def drawText(displayWH, text, sz, x, y):
     glDisable(GL_TEXTURE_2D)
     glDisable(GL_BLEND)
     popGL()
+
+#draws face with color texture
+def drawFace(vertices, inds, color):
+    i=genColorTexture(color)
+    glEnable(GL_TEXTURE_2D)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    glEnable(GL_BLEND)
+    glBindTexture(GL_TEXTURE_2D, i)
+    glBegin(GL_POLYGON)
+    for i in range(len(inds)):
+        ix, iy = [[0, 0], [1, 0], [1, 1]][i%3]
+        glTexCoord2f(ix, iy)
+        glVertex3f(*vertices[inds[i]])
+    glEnd()
+    glDisable(GL_TEXTURE_2D)
+    glDisable(GL_BLEND)
