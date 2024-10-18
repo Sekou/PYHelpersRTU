b28e450d16f830a81d463e3acb908ce0ecc009d9
Sekou <sekoudiane1990@gmail.com>
2024-10-18 15:33:59 +0300

    Update tf_net_example.py

--- tf_net_example.py
+++ tf_net_example.py
@@ -9,5 +9,17 @@ model = tf.keras.Sequential([
 ])
 model.summary()
 
-result=model.predict([[250, 170, 0.5]])
+model.compile(optimizer='adam', loss='mae')
+
+X_train=[[0,0,0], [0.5, 0.5, 0.5], [1, 1, 1]]
+Y_train=[[1], [2], [3]]
+X_val=[[0.1,0.1,0.1], [0.7, 0.7, 0.7], [1.3, 1.3, 1.3]]
+Y_val=[[1.2], [2.4], [3.6]]
+
+losses = model.fit(X_train, Y_train,
+                   validation_data=(X_val, Y_val),
+                   batch_size=1,
+                   epochs=15 )
+
+result=model.predict([[0.4, 0.4, 0.4]])
 print(result)
