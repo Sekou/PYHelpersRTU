c6b1f894fd79965bcc9ac4ac665e8a4f2a792275
Sekou <sekoudiane1990@gmail.com>
2024-12-09 06:55:31 +0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,4 +1,4 @@
-def probSel(probs): #вероятностный выбор индекса элемента
+def prob_sel(probs): #вероятностный выбор индекса элемента
     m, s, r=sum(probs), 0, np.random.rand()
     if m==0: return np.random.randint(len(probs))
     for i in range(len(probs)):
@@ -6,35 +6,47 @@ def probSel(probs): #вероятностный выбор индекса эле
         if s>=r: return i
     return -1
 
-def readPts(filename): #чтение массива целочисленных точек
+def read_pts(filename): #чтение массива целочисленных точек
     with open(filename, "r") as f:
         return [[int(v) for v in l.split()] for l in f.readlines()]
 
-def rotSegm(segm, ang): #центральный поворот отрезка на угол
+def rot_segm(segm, ang): #центральный поворот отрезка на угол
     c=np.mean(segm, axis=0)
     v1=np.subtract(segm[0], c)
     v2=np.subtract(segm[1], c)
     return list(np.add([rot(v1, ang), rot(v2, ang)], c))
 
-def ptSegmDist(p, p1, p2): #расстояние от точки до отрезка
+def pt_segm_dist(p, p1, p2): #расстояние от точки до отрезка
     dx, dy = np.subtract(p2, p1)
     k = dy / (0.0000001 if dx==0 else dx)
     b = p1[1] - k * p1[0]
     return np.abs(-k * p[0] + p[1] - b) / math.sqrt(k * k + 1)
 
-def proj(segm, pt): #точка-проекция точки на отрезок
+def project_pt(segm, pt): #точка-проекция точки на отрезок
     v2=np.subtract(pt, segm[0], dtype=float)
     v1=np.subtract(segm[1], segm[0], dtype=float)
     v1_=v1/np.linalg.norm(v1)
     L2=np.dot(v1_, v2)
     return segm[0] + L2*v1_
 
-def projCheck(segm, pt): #проверка попадания проецирцемой точки внетрь отрезка
+def check_proj(segm, pt): #проверка попадания проецирцемой точки внетрь отрезка
     v2=np.subtract(pt, segm[0], dtype=float)
     v1=np.subtract(segm[1], segm[0], dtype=float)
     L1=np.linalg.norm(v1)
     return 0<=np.dot(v1/L1, v2)<=L1
 
-def intersect(A,B,C,D): #проверка пересечения двух отрезков
+def check_intersection(A,B,C,D): #проверка пересечения двух отрезков
     ccw = lambda A, B, C: (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x)
     return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+
+def get_segm_intersection(A, B, C, D):
+    (x1, y1), (x2, y2), (x3, y3), (x4, y4) = A, B, C, D
+    denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+    if denom == 0: return None  # Отрезки параллельны или совпадают
+    t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / denom
+    u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / denom
+    if 0 <= t <= 1 and 0 <= u <= 1:
+        intersection_x = x1 + t * (x2 - x1)
+        intersection_y = y1 + t * (y2 - y1)
+        return (intersection_x, intersection_y)
+    return None
