fbbe846b80eb3196a5b3498c2ab070b5bd4e7dff
Sekou <sekoudiane1990@gmail.com>
2025-07-23 02:06:18 +0300

    Update animation2d.py

--- animation2d.py
+++ animation2d.py
@@ -4,7 +4,7 @@ import numpy as np
 import math
 
 pygame.font.init()
-def drawText(screen, s, x, y, sz=20, color=(0,0,0)): #отрисовка текста
+def draw_text(screen, s, x, y, sz=20, color=(0,0,0)): #отрисовка текста
     font = pygame.font.SysFont('Comic Sans MS', sz)
     screen.blit(font.render(s, True, (0,0,0)), (x,y))
 
@@ -12,25 +12,25 @@ def rot(v, ang): #поворот вектора на угол
     s, c = math.sin(ang), math.cos(ang)
     return [v[0] * c - v[1] * s, v[0] * s + v[1] * c]
 
-def limAng(ang): #ограничение угла в пределах +/-pi
+def lim_ang(ang): #ограничение угла в пределах +/-pi
     while ang > math.pi: ang -= 2 * math.pi
     while ang <= -math.pi: ang += 2 * math.pi
     return ang
 
-def rotArr(vv, ang): # функция для поворота массива на угол
+def rot_arr(vv, ang): # функция для поворота массива на угол
     return [rot(v, ang) for v in vv]
 
 def dist(p1, p2): #расстояние между точками
     return np.linalg.norm(np.subtract(p1, p2))
 
-def drawRotRect(screen, color, pc, w, h, ang): #точка центра, ширина высота прямоуг и угол поворота прямогуольника
+def draw_rot_rect(screen, color, pc, w, h, ang): #точка центра, ширина высота прямоуг и угол поворота прямогуольника
     pts = [
         [- w/2, - h/2],
         [+ w/2, - h/2],
         [+ w/2, + h/2],
         [- w/2, + h/2],
     ]
-    pts = rotArr(pts, ang)
+    pts = rot_arr(pts, ang)
     pts = np.add(pts, pc)
     pygame.draw.polygon(screen, color, pts, 2)
 
@@ -41,6 +41,7 @@ def main():
     pygame.display.set_caption('Animation 2D')
     timer = pygame.time.Clock()
     fps = 20
+    dt=1/fps   
 
     while True:
         for ev in pygame.event.get():
@@ -48,13 +49,11 @@ def main():
                 sys.exit(0)
             if ev.type == pygame.KEYDOWN:
                 if ev.key == pygame.K_r:
-                    print("Hi")
-
-        dt=1/fps        
+                    print("Hi")     
 
         screen.fill((255, 255, 255))     
 
-        drawText(screen, f"Test = {1}", 5, 5)
+        draw_text(screen, f"Test = {1}", 5, 5)
 
         pygame.display.flip()
         timer.tick(fps)
