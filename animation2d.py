edd09a167b28480737ed97fcfafe835369887d59
Sekou <sekoudiane1990@gmail.com>
2025-07-24 11:41:51 +0300

    Update animation2d.py

--- animation2d.py
+++ animation2d.py
@@ -4,9 +4,8 @@ import numpy as np
 import math
 
 pygame.font.init()
-def draw_text(screen, s, x, y, sz=20, color=(0,0,0)): #отрисовка текста
-    font = pygame.font.SysFont('Comic Sans MS', sz)
-    screen.blit(font.render(s, True, (0,0,0)), (x,y))
+def draw_text(screen, s, x, y, sz=15, color=(0, 0, 0)):  # отрисовка текста
+    screen.blit(pygame.font.SysFont('Comic Sans MS', sz).render(s, True, color), (x, y))
 
 def rot(v, ang): #поворот вектора на угол
     s, c = math.sin(ang), math.cos(ang)
