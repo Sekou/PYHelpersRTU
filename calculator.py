e45de21a6eb77400a522d86243e8dde7be0e5210
Sekou <sekoudiane1990@gmail.com>
2025-08-21 21:43:00 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import scrolledtext, messagebox
+import re
+
+def proc_format():
+    input = txt_input_query.get("1.0", tk.END).strip()
+    txt_output.delete(1.0, tk.END)
+    if re.fullmatch(r"^[\d+\-*/().]+$", input) is not None:
+        res=eval(input)
+        txt_output.insert(tk.END, res)
+
+# создаем окно
+root = tk.Tk()
+root.title("Расчет")
+# создаем текстовое поле для ввода названия
+lbl_instruct = tk.Label(root, text="Введите выражение:")
+lbl_instruct.pack(pady=5)
+
+txt_input_query = tk.Text(root, height=2, width=70)
+txt_input_query.pack(pady=5)
+
+# создаем кнопку обработки
+btn_convert = tk.Button(root, text="Рассчитать", command=proc_format)
+btn_convert.pack(pady=5)
+
+# место для вывода результата
+lbl_instruct = tk.Label(root, text="Рассчитанный результат: ")
+lbl_instruct.pack(pady=5)
+
+txt_output = tk.Text(root, height=2, width=70)
+txt_output.pack(pady=5)
+
+# запуск приложения
+root.mainloop()
