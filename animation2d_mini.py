8402e6895f7d693292bf11efcf2c57e6a13c727c
Sekou <sekoudiane1990@gmail.com>
2025-11-03 12:56:24 +0300

    Update animation2d_mini.py

--- animation2d_mini.py
+++ animation2d_mini.py
@@ -1,6 +1,4 @@
-import sys, pygame
-import numpy as np
-import math
+import sys, pygame, numpy as np
 
 pygame.font.init()
 def draw_text(screen, s, x, y, sz=15, с=(0, 0, 0)):  # отрисовка текста
@@ -12,19 +10,15 @@ def main():
     screen = pygame.display.set_mode(sz)
     pygame.display.set_caption('Animation 2D')
     timer = pygame.time.Clock()
-    fps = 20
-    dt=1/fps
+    fps = 20; dt=1/fps
     
     while True:
         for ev in pygame.event.get():
-            if ev.type==pygame.QUIT:
-                sys.exit(0)
+            if ev.type==pygame.QUIT: sys.exit(0)
             if ev.type == pygame.KEYDOWN:
-                if ev.key == pygame.K_r:
-                    print("Hi")                
+                if ev.key == pygame.K_r: print("Hi")                
 
         screen.fill((255, 255, 255))     
-
         draw_text(screen, f"Test = {1}", 5, 5)
 
         pygame.display.flip()
