6ef8d2bdd05dc40d0c968e1afebd6b73c0344955
Sekou <sekoudiane1990@gmail.com>
2025-12-02 00:25:01 +0300

    Update cam_move_3d.py

--- cam_move_3d.py
+++ cam_move_3d.py
@@ -11,28 +11,21 @@ class Camera:
     hFOV = 45  # inner params
     hRes, vRes = 800, 600
     nearZ, farZ = 0.1, 50
-    kPI = np.pi / 180
+    kPI, author = np.pi / 180, "S. Diane, 2025"
     swapCS = np.array([[0, -1, 0, 0], [0, 0, 1, 0], [-1, 0, 0, 0], [0, 0, 0, 1]])
-
     def save_pos(self): self.coords = self.x, self.y, self.z, self.yaw, self.pitch, self.roll
-
     def restore_pos(self): self.x, self.y, self.z, self.yaw, self.pitch, self.roll = self.coords
-
     def move_local(self, dx, dy, dz):
         M = self.calc_view_matrix()[:3, :3]
         M_ = np.linalg.inv(M)
         dx, dy, dz = M_ @ [dx, dy, dz]
         self.x, self.y, self.z = self.x+dx, self.y+dy, self.z+dz
-
     def calc_proj_matrix(self):
-        ar = self.hRes / self.vRes
-        tn = tan(self.hFOV * self.kPI / 2)
-        d1 = self.nearZ - self.farZ
-        d2 = self.nearZ + self.farZ
+        ar, tn = self.hRes / self.vRes, tan(self.hFOV * self.kPI / 2)
+        d1, d2 = self.nearZ - self.farZ, self.nearZ + self.farZ
         A, B, C, D = 1 / ar / tn, 1 / tn, d2 / d1, 2 * self.farZ * self.nearZ / d1
         mproj = np.array([[A, 0, 0, 0], [0, B, 0, 0], [0, 0, C, D], [0, 0, -1, 0]])
         return mproj
-
     def calc_view_matrix(self, swap_axes=True):
         # positive world shift -> negative camera shift
         X, Y, Z = -self.x, -self.y, -self.z
@@ -46,93 +39,63 @@ class Camera:
         # calc full view transformation
         mshift = [[1, 0, 0, X], [0, 1, 0, Y], [0, 0, 1, Z], [0, 0, 0, 1]]
         camMatrix = np.array(mpit) @ mrol @ myaw @ mshift
-        if swap_axes: camMatrix = self.swapCS @ camMatrix
-        return camMatrix
-
+        return self.swapCS @ camMatrix if swap_axes else camMatrix
     def calc_screen_matrix(self, swap_y=True):
         k = -1 if swap_y else 1
         return np.array([[0.5 * self.hRes, 0, 0, 0.5 * self.hRes],
                          [0, 0.5 * self.vRes, 0, k * 0.5 * self.vRes],
                          [0, 0, 1, 0], [0, 0, 0, 1]])
-
     def transf_DC_to_screen(self, pDC):
-        pNDC = pDC / pDC[-1]
-        S = self.calc_screen_matrix()
-        return (S @ pNDC)[:2]  # x, y for a pixel
-
+        return (self.calc_screen_matrix() @ (pDC / pDC[-1]))[:2]  # x, y for a pixel
     def transf_pts_from_world_to_screen(self, pts, M=None):
-        res = []
         if M is None: M = self.calc_proj_matrix() @ self.calc_view_matrix()
-        for p in pts:
-            pDC = M @ [p[0], p[1], p[2], 1]
-            pScreen = self.transf_DC_to_screen(pDC)
-            res.append(tuple(pScreen))
-        return res
-
+        return [tuple(self.transf_DC_to_screen(M @ [p[0], p[1], p[2], 1])) for p in pts]
     def get_info(self):
         return f"x {self.x:.2f}, y {self.y:.2f}, z {self.z:.2f}," \
                f" rl {self.roll:.2f}, pt {self.pitch:.2f}, yw {self.yaw:.2f}"
-
     def draw_world_axes(self):
         for v in np.array([(1, 0, 0), (0, 1, 0), (0, 0, 1)]):
-            glColor(v), glBegin(GL_LINES)
-            glVertex3fv((0, 0, 0))
-            glVertex3fv(v)
-            glEnd()
-
+            glColor(v), glBegin(GL_LINES), glVertex3fv((0, 0, 0)), glVertex3fv(v), glEnd()
     def draw_world_axes_ortho_2d(self, sz=20, x0y0=(30,30)): #requires: glOrtho(0, display[0], 0, display[1], -1, 1)
         vv_global=cc=[(1, 0, 0), (0, 1, 0), (0, 0, 1)]
         MV=self.calc_view_matrix()[:3,:3]
         pp, p0=[(MV@v)[:2] for v in vv_global], (MV@[0, 0, 0])[:2]
         for c, p in zip(cc, pp):
-            glColor(c), glBegin(GL_LINES)
-            glVertex2fv(x0y0), glVertex2fv(sz*p+x0y0)
-            glEnd()
-
+            glColor(c), glBegin(GL_LINES), glVertex2fv(x0y0), glVertex2fv(sz*p+x0y0), glEnd()
     def draw_cam_axes_ortho_2d(self, sz=20, x0y0=(30,30)): #requires: glOrtho(0, display[0], 0, display[1], -1, 1)
         vv_local=cc=[(1, 0, 0), (0, 1, 0), (0, 0, 1)]
         vv_global = self.swapCS[:3,:3]@vv_local
         MV=self.calc_view_matrix()[:3,:3]
         pp, p0=[(MV@v)[:2] for v in vv_global], (MV@[0, 0, 0])[:2]
         for c, p in zip(cc, pp):
-            glColor(c), glBegin(GL_LINES)
-            glVertex2fv(x0y0), glVertex2fv(sz*p+x0y0)
-            glEnd()
+            glColor(c), glBegin(GL_LINES), glVertex2fv(x0y0), glVertex2fv(sz*p+x0y0), glEnd()
 
 #Camera navigation in simple 3D scene, 2023-2025, S. Diane
 import pygame
 
+def show_mat(mat, prec=5): return str([[round(x, prec) for x in r] for r in mat])
+
 @np.vectorize
 def F(x,y): return 0.03*np.sin(5*x)*np.sin(5*y)
 def gen_surface_points(n):
-    r = 2*np.arange(n) / (n-1) - 1
-    inps=np.meshgrid(r, r)
-    res=np.moveaxis(np.array([*inps, F(*inps)]), 0, -1)
-    return res.reshape((n*n,3))
-
-def show_mat(mat, prec=5):
-    res=[[round(x, prec) for x in r] for r in mat]
-    return str(res)
+    inps=np.meshgrid(2*np.arange(n)/(n-1)-1, 2*np.arange(n)/(n-1)-1)
+    return np.moveaxis(np.array([*inps, F(*inps)]), 0, -1).reshape((n*n,3))
 
 def draw_pts(points):
-    start = glColor((1, 1, 1)), glBegin(GL_POINTS)
+    glPointSize(5.0), glColor((1, 1, 1)), glBegin(GL_POINTS)
     for v in points: glVertex3fv(v)
     glEnd()
 
 points = gen_surface_points(10)
 display = (800, 600)
 
-def main():
+if __name__=="__main__":
     pygame.init()
     screen = pygame.display.set_mode(display, pygame.DOUBLEBUF|pygame.OPENGL)
-
     cam=Camera()
     cam.x, cam.y, cam.z = -5,0,1
     cam.save_pos()
-
-    ind_frame=0
-    last_key=0
-    last_info=""
+    ind_frame, last_key, last_info = 0, 0, ""
 
     while True:
         for event in pygame.event.get():
@@ -153,12 +116,10 @@ def main():
                     for p in points:
                         q=M@[*p, 1]; q=q[:2]/q[-1]
                         points2d.append(list(q))
-                    print("PTS 3d: ", points)
-                    print("PTS 2d: ", points2d)
-
-        keys = pygame.key.get_pressed()  # get the state of all keys
+                    print("PTS 3d: ", points, "\nPTS 2d: ", points2d)
 
         # camera motion
+        keys = pygame.key.get_pressed()  # get the state of all keys
         if keys[pygame.K_UP]: cam.move_local(0, 0, -0.1)
         if keys[pygame.K_DOWN]: cam.move_local(0, -0, 0.1)
         if keys[pygame.K_RIGHT]: cam.yaw-=1
@@ -180,20 +141,13 @@ def main():
         ci=cam.get_info()
         if ci!=last_info: print(f"i={ind_frame}: ", last_info:=ci)
 
-        glPointSize(5.0)
-
-        #1 projection matrix
-        MP = np.transpose(cam.calc_proj_matrix()) #column-major order
-        glMultMatrixf(MP)
-
-        #2 view matrix
-        MV = np.transpose(cam.calc_view_matrix()) #column-major order
-        glMultMatrixf(MV)
+        glMultMatrixf(MP:=np.transpose(cam.calc_proj_matrix())) # 1 projection matrix (column-major order)
+        glMultMatrixf(MV:=np.transpose(cam.calc_view_matrix())) # 2 view matrix (column-major order)
+        if ind_frame == 0: print(f"MP={MP}\n", f"MV={MV}\n")
 
         # drawing points
         draw_pts(points)
         cam.draw_world_axes()
-        if ind_frame == 0: print(f"MP={MP}\n", f"MV={MV}\n")
 
         glMatrixMode(GL_MODELVIEW), glLoadIdentity()
         glOrtho(0, display[0], 0, display[1], -1, 1)
@@ -203,5 +157,3 @@ def main():
         pygame.display.flip()
         pygame.time.wait(20)
         ind_frame+=1
-
-if __name__=="__main__": main()
