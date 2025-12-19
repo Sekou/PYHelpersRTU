ec188af8cadc733f3fae31680f5aceb3d03e4dd3
Sekou <sekoudiane1990@gmail.com>
2025-12-19 14:06:19 +0300

    Update lunar2d.py

--- lunar2d.py
+++ lunar2d.py
@@ -1,33 +1,22 @@
-import sys, pygame
-import numpy as np
-import math
+import sys, pygame, numpy as np, math
 
 pygame.font.init()
 def drawText(screen, s, x, y, sz=20, color=(0,0,0)): #отрисовка текста
     font = pygame.font.SysFont('Comic Sans MS', sz)
     surf=font.render(s, True, (0,0,0))
     screen.blit(surf, (x,y))
-
+def lim_ang(ang, arc=3.141592653589793): # ограничение угла в пределах +/-pi
+    ang=ang%(2*arc); return ang + (2*arc if ang<-arc else -2*arc if ang>arc else 0)
 def rot(v, ang): #поворот вектора на угол
     s, c = math.sin(ang), math.cos(ang)
     return [v[0] * c - v[1] * s, v[0] * s + v[1] * c]
-
-def limAng(ang): #ограничение угла в пределах +/-pi
-    while ang > math.pi: ang -= 2 * math.pi
-    while ang <= -math.pi: ang += 2 * math.pi
-    return ang
-
 def rotArr(vv, ang): # функция для поворота массива на угол
     return [rot(v, ang) for v in vv]
-
 def dist(p1, p2): #расстояние между точками
     return np.linalg.norm(np.subtract(p1, p2))
-
 def drawRotRect(screen, color, pc, w, h, ang): #точка центра, ширина высота прямоуг и угол поворота прямогуольника
     pts = [[- w/2, - h/2], [+ w/2, - h/2], [+ w/2, + h/2], [- w/2, + h/2]]
-    pts = rotArr(pts, ang)
-    pts = np.add(pts, pc)
-    pygame.draw.polygon(screen, color, pts, 2)
+    pygame.draw.polygon(screen, color, np.add(rotArr(pts, ang), pc), 2)
 
 sz = (800, 600)
 
@@ -54,7 +43,6 @@ class LunarModule:
         pygame.draw.polygon(screen, color, pts, 2)
         for i,x in enumerate([22, 7, -7, -22]):
             self.draw_flame(screen, np.add(self.getPos(),rot([x,29], self.ang)), self.gas[i])
-
     def sim(self, dt):
         if self.collision: return
         gLunar=5
@@ -74,7 +62,6 @@ class LunarModule:
         self.vy += ay * dt
         self.x += self.vx * dt
         self.y += self.vy * dt
-
     def checkCollision(self, terrain):
         pts=rotArr(self.pts, self.ang)+np.array(self.getPos())
         pts2=terrain.getPts()
@@ -110,7 +97,7 @@ class Terrain:
             pts.append([x, y])
         return pts
 
-def main():
+if __name__=="__main__":
     screen = pygame.display.set_mode(sz)
     timer = pygame.time.Clock()
     fps = 20
@@ -123,29 +110,21 @@ def main():
             if ev.type==pygame.QUIT:
                 sys.exit(0)
             if ev.type == pygame.KEYDOWN:
-                if ev.key == pygame.K_w:
-                    lm.gas=[0.5, 0.5, 0.5, 0.5]
-                if ev.key == pygame.K_d:
-                    lm.gas=[0, 0, 0.1, 0.1]
-                if ev.key == pygame.K_a:
-                    lm.gas=[0.1, 0.1, 0, 0]
+                if ev.key == pygame.K_w: lm.gas=[0.5, 0.5, 0.5, 0.5]
+                if ev.key == pygame.K_d: lm.gas=[0, 0, 0.1, 0.1]
+                if ev.key == pygame.K_a: lm.gas=[0.1, 0.1, 0, 0]
 
         dt=1/fps
 
         lm.sim(dt)
         lm.checkCollision(terrain)
-
         lm.gas=0.9*np.array(lm.gas)
-
         screen.fill((255, 255, 255))
         terrain.draw(screen)
         lm.draw(screen)
-
         drawText(screen, f"Test = {1}", 5, 5)
 
         pygame.display.flip()
         timer.tick(fps)
-
-main()
 
 #template file by S. Diane, RTU MIREA, 2024
