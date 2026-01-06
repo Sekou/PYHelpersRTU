415ce12318686eef53f383d477b721fab07cde83
Sekou <sekoudiane1990@gmail.com>
2026-01-06 04:01:45 +0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -176,8 +176,7 @@ def get_insert_ind(points_, mouse_pos): # поиск отрезка для вс�
 
 def project_pt(segm, pt): # точка-проекция точки на отрезок
     v1, v2=np.subtract(segm[1], segm[0], dtype=float), np.subtract(pt, segm[0], dtype=float)
-    v1_=v1/np.linalg.norm(v1)
-    return segm[0] + np.dot(v1_, v2)*v1_
+    return segm[0] + np.dot(v1, v2)*v1/np.dot(v1,v1)
 
 def check_proj(segm, pt): # проверка попадания проецирцемой точки внетрь отрезка
     v2=np.subtract(pt, segm[0], dtype=float)
