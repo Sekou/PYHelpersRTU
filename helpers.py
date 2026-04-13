bac9b9ecd3594ac26445125e34a10acd423f1965
Sekou <sekoudiane1990@gmail.com>
2026-04-13 13:34:29 +0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -519,9 +519,8 @@ class Task: #задача, выполняемая роботом в течени
     def run(self, robot, objs, t, dt): print(f"{t:.2f}: {self.name}")
     def draw(self, screen): pass
 
-class TaskGoTo(Task): #задача движения к точке
-    def __init__(self, goal): 
-		_, self.name, self.goal = super().__init__(), self.__class__.__name__, self.goal=goal
+class TaskGoTo(Task): #задача движения к точке #t=TaskGoTo((100,100))
+    def __init__(self, goal): _, self.goal = super().__init__(), goal
     def run(self, robot, objs, t, dt):
         print(f"{t:.2f}: {self.name}"), robot.goto(self.goal, dt)
         if dist(robot.get_pos(), self.goal)<45: self.finished, _=True, robot.stop()
